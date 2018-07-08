b5160a40acba7773c608e973c327a5616b06bbc1
lEmanl <emanuelurbina@live.com>
2018-07-07 23:00:58 -0500

    dived the code into class, functions, and main

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import numpy
 pygame.init()
 #idk what a python file should look like so yeh
 
+"""classes"""
+
+class aight(object):
+    pass
+
+"""functions and variables"""
+
+
+"""main"""
+
 def main():
     pass
 
