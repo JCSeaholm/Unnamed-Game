e45cc0c8908de0df2dfa14f7ac9345b32cc5f895
JCSeaholm <40710976+JCSeaholm@users.noreply.github.com>
2018-07-10 18:27:14 -0500

    A settings module
    This should deal with all of the settings to keep our main script from getting cluttered.

---
+++ settings.py
@@ -0,0 +1,17 @@
+#provides OS info (screen size)
+import Tkinter
+#allows user's settings to be saved in a JSON file to be retrieved later
+import json
+
+#the default screen width and height are determined by the actual screen's dimensions
+root = Tkinter.Tk()
+screenWidth = root.winfo_screenwidth()
+screenHeight = root.winfo_screenheight()
+
+#use a JSON file to make it easier to save settings between sessions
+userSettingsFile = open('userSettings.json')
+userSettings = json.load(userSettings)
+
+#I'll have to mess with this, never used JSON before
+userSetWidth = userSettings.width
+userSetHeight = userSettings.height
